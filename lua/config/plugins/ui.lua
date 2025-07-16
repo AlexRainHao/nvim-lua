3d0c806375299dfac8b06d91368173ce66e931eb
AlexRainHao <yuanyuhaoyyh@gmail.com>
2025-07-16 22:50:18 +0800

    feat: optional specific color scheme

--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -1,5 +1,5 @@
-return {
-  {
+local theme_config = {
+  everforest = {
     "neanias/everforest-nvim",
     version = false,
     lazy = false,
@@ -8,5 +8,34 @@ return {
       vim.cmd([[colorscheme everforest]])
     end,
   },
-  { "nvim-tree/nvim-web-devicons", opts = {} },
+  onedark = {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'warm'
+      }
+      -- Enable theme
+      require('onedark').load()
+    end
+  }
 }
+
+function determine_teme()
+  local _prefer_theme = require("specific").prefer_theme
+
+  if not theme_config[_prefer_theme] then
+    error("theme " .. _prefer_theme .. "not supported")
+  end
+
+  return theme_config[_prefer_theme]
+end
+
+local M = {}
+
+M.config = {
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+  determine_teme(),
+}
+
+return M
--- lua/specific/init.lua
+++ lua/specific/init.lua
@@ -1,1 +1,8 @@
 vim.g.python3_host_prog = "/usr/bin/python3"
+
+M = {}
+
+-- `everforest` | `onedark`
+M.prefer_theme = "onedark"
+
+return M
