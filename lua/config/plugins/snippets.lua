60da239688f7eee7ce3fa483da315abd67d15482
AlexRainHao <yuanyuhaoyyh@gmail.com>
2025-07-18 00:24:52 +0800

    feat: activate `ultisnips` folder in plugin of `ultisnips`

--- lua/config/plugins/snippets.lua
+++ lua/config/plugins/snippets.lua
@@ -1,9 +1,10 @@
 return {
   "SirVer/ultisnips",
+  event = { 'InsertEnter' },
   dependencies = {
     "honza/vim-snippets",
   },
   config = function()
-    vim.g.UltiSnipsSnippetDirectories = { "Ultisnips", "~/.config/nvim/Ultisnips" }
+    vim.g.UltiSnipsSnippetDirectories = { "UltiSnips", "~/.config/nvim/Ultisnips" }
   end
 }
