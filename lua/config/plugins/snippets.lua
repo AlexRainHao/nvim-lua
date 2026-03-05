dcaf168a9e1111ae9934a15f7547185bee5bf204
AlexRain <yuanyuhaoyyh@gmail.com>
2026-03-05 10:43:08 +0800

    feat: docstring generator plugin on `vim-doge`

--- lua/config/plugins/snippets.lua
+++ lua/config/plugins/snippets.lua
@@ -1,14 +1,24 @@
 return {
-  'SirVer/ultisnips',
-  event = { 'InsertEnter' },
-  dependencies = {
-    'honza/vim-snippets',
+  {
+    'SirVer/ultisnips',
+    event = { 'InsertEnter' },
+    dependencies = {
+      'honza/vim-snippets',
+    },
+    config = function()
+      vim.g.UltiSnipsSnippetDirectories = {
+        'UltiSnips',
+        'vim-snippets',
+        '~/.config/nvim/Ultisnips',
+      }
+    end,
   },
-  config = function()
-    vim.g.UltiSnipsSnippetDirectories = {
-      'UltiSnips',
-      'vim-snippets',
-      '~/.config/nvim/Ultisnips',
-    }
-  end,
+  {
+    'kkoomen/vim-doge',
+    build = ':call doge#install()',
+    config = function()
+      vim.g.doge_mapping = '<Leader>d'
+      vim.g.doge_doc_standard_python = 'google'
+    end,
+  },
 }
