177b7e149242b6839111467fb4ff00261b4613b1
AlexRain <yuanyuhaoyyh@gmail.com>
2026-03-24 14:14:52 +0800

    feat: plugin on vue comment

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,13 +1,24 @@
 return {
-  'tomtom/tcomment_vim',
-  event = 'BufRead',
-  config = function()
-    vim.g.tcomment_maps = true
-    vim.g.tcomment_textobject_inlinecomment = ''
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+    event = 'BufRead',
+    config = function()
+      require('Comment').setup({
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      })
 
-    vim.cmd([[
+      vim.cmd([[
       nmap <Leader>/<Leader> gcc
       vmap <Leader>/ gc
     ]])
-  end,
+    end
+  },
 }
